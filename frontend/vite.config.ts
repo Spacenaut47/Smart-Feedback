9e3333fd7bc6585665fad6af89917c21c49733aa
Spacenaut47 <sohammondal81@gmail.com>
2025-07-11 06:40:57 +0000

    Clean vite config for Netlify

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build'
+    outDir: 'dist'
   },
   base: './' // <--- ADD THIS LINE to fix relative paths
 })
+
+
